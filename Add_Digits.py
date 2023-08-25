9e50c416084b0f78b6dd25f7c9b464aaf6ce5862
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-08-25 14:42:45 +0530

    n=int(input())
    while True:
        s = 0
        q = n
        while(q!=0):
            r=q%10
            s=s+r
            q=q//10
        if s>9:
            n = s
        else:break
        
    print(s)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,3 +1,14 @@
 n=int(input())
-n=n%9
-print(n)
+
+while True:
+    s = 0
+    q = n
+    while(q!=0):
+        r=q%10
+        s=s+r
+        q=q//10
+    if s>9:
+        n = s
+    else:break
+    
+print(s)
