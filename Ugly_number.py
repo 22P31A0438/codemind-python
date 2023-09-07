e3c0222d986f8d95943969e1e0b702fa44181295
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-09-07 22:15:08 +0530

    def ug(n):         if n == 0:             return False         for i in [2, 3, 5]:             while n % i == 0:                 n /= i         if(n==1):             return 1         else:             return 0 a=int(input()) if(ug(a)):     print("Ugly Number") else:     print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,15 @@
+def ug(n):
+        if n == 0:
+            return False
+        for i in [2, 3, 5]:
+            while n % i == 0:
+                n /= i
+        if(n==1):
+            return 1
+        else:
+            return 0
+a=int(input())
+if(ug(a)):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
