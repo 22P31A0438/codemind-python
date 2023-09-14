aac2295985e995ceb1181381740dde65db360590
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-09-14 21:43:45 +0530

    def clo(n):     j=n     s=0     while(j>0):         r=j%10         j=j//10         s=s*10+r     if(s==n):         return True     else:         return False n=int(input()) i=n+1 j=n-1 while not clo(i) and not clo(j):     i=i+1     j=j-1 if(clo(i) and clo(j)):     if(abs(n-i)==abs(i-n)):         print(j,i)     else:         if(abs(i-n)>abs(n-j)):             print(j)         else:             print(i) elif(clo(i)):     print(i) else:     print(j)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,4 +1,4 @@
-def clopal(n):
+def clo(n):
     j=n
     s=0
     while(j>0):
@@ -12,19 +12,18 @@ def clopal(n):
 n=int(input())
 i=n+1
 j=n-1
-while not clopal(i) and not clopal(j):
+while not clo(i) and not clo(j):
     i=i+1
     j=j-1
-if(clopal(i) and clopal(j)):
-    if(abs(i-n)==abs(n-i)):
+if(clo(i) and clo(j)):
+    if(abs(n-i)==abs(i-n)):
         print(j,i)
     else:
         if(abs(i-n)>abs(n-j)):
             print(j)
         else:
             print(i)
-elif(clopal(i)):
-        print(i)
+elif(clo(i)):
+    print(i)
 else:
     print(j)
-        
