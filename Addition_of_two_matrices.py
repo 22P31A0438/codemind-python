296166d60f9b3c87b12a300242fa47606ebb32a9
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-11-09 15:09:14 +0530

    n=int(input()) m=[list(map(int,input().split())) for i in range(n)] r=[list(map(int,input().split())) for i in range(n)] c=[m[j][k]+r[j][k] for j in range(n) for k in range(n)] ind = 0 for i in range(n):     for j in range(n):         print(c[ind],end=" ")         ind+=1     print()

--- Addition_of_two_matrices.py
+++ Addition_of_two_matrices.py
@@ -1,10 +1,10 @@
 n=int(input())
 m=[list(map(int,input().split())) for i in range(n)]
 r=[list(map(int,input().split())) for i in range(n)]
+c=[m[j][k]+r[j][k] for j in range(n) for k in range(n)]
+ind = 0
 for i in range(n):
     for j in range(n):
-        m[i][j]=m[i][j]+r[i][j]
-for i in m:
-    for j in i:
-        print(j,end=" ")
+        print(c[ind],end=" ")
+        ind+=1
     print()
